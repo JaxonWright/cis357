87ead55bc14bb0bbb7ee6796c63bb0e20c5497f8
Jaxon <jaxon@starscream.local>
2017-01-18 22:36:05 -0500

    add HW1. Finished problem 1

---
+++ hw1/hw1_playground.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+// CIS 357 - HW1 - Jaxon Wright and Kent Sinclair
+
+//Problem 1
+let myString = "hello"
+var cost = 3.14
+let cnt: Int = 2
+var shouldBe = true
+let intConst1 = 0xB //11
+let intConst2 = 0b1010 //10
+
+//Problem 2
+
+//Problem 3
+
+//Problem 4
+
+//Problem 5
+
+//Problem 6
+
+//Problem 7
+
+//Problem 8
+
+//Problem 9
+
+//Problem 10
+
+//Problem 11
+
+//Problem 12
+
+//Problem 13
+
+//Problem 14
+
+//Problem 15
+
+//Problem 16
+
+//Problem 17
+
+//Problem 18
+
+//Problem 19
+
+//Problem 20
