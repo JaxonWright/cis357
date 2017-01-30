d7f33f8f29ab14fd3911fe022c785c0d74c872ee
Kent <kent.sinclair@gmail.com>
2017-01-29 19:22:54 -0500

    round the answers using string formating and make the keyboard disappear when you click outside of the textfield

--- hw2/HaversineCalculator/HaversineCalculator/ViewController.swift
+++ hw2/HaversineCalculator/HaversineCalculator/ViewController.swift
@@ -6,8 +6,14 @@
 //  Copyright © 2017 KJAX. All rights reserved.
 //
 
+
 import UIKit
 import CoreLocation
+
+
+// we need to disable the calculate button unless all UITextFields are full
+// http://stackoverflow.com/questions/29448185/how-can-i-disable-a-button-until-text-in-all-uitextfields-has-been-entered-in-sw
+// 
 
 class ViewController: UIViewController {
     @IBOutlet weak var distanceLabel: UILabel!
@@ -21,7 +27,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // make the keyboard disappear when you click outside of the textfield
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,8 +47,10 @@ class ViewController: UIViewController {
                 let c1 = CLLocation(latitude: Double(latitudeP1.text!)!, longitude: Double(longitudeP1.text!)!)
                 let c2 = CLLocation(latitude: Double(latitudeP2.text!)!, longitude: Double(longitudeP2.text!)!)
                 
-                distanceLabel.text?.append(" \(calcDistance(loc1: c1, loc2: c2)) kilometers")
-                bearingLabel.text?.append(" \(c1.bearingToPoint(point: c2)) degrees")
+                distanceLabel.text = "Distance: "
+                bearingLabel.text = "Bearing: "
+                distanceLabel.text?.append(" \(String(format: "%.2f", calcDistance(loc1: c1, loc2: c2))) kilometers")
+                bearingLabel.text?.append(" \(String(format: "%.2f", c1.bearingToPoint(point: c2))) degrees")
             case "Clear":
                 clearAll()
             default:
@@ -60,6 +73,12 @@ class ViewController: UIViewController {
         latitudeP2.text = ""
         longitudeP1.text = ""
         longitudeP2.text = ""
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
 }
