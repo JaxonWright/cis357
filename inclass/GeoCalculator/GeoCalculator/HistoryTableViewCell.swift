fad80925fcceb744916fcf5ab73b404ef673a860
Kent Sinclair <kent.sinclair@gmail.com>
2017-03-29 13:28:04 -0400

    unclean branch commit

---
+++ inclass/GeoCalculator/GeoCalculator/HistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.swift
+//  GeoCalculator
+//
+//  Created by Kent Sinclair on 3/20/17.
+//  Copyright © 2017 Jonathan Engelsma. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ inclass/ic-hw6/GeoCalculator/GeoCalculator/LocationSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationSearchViewController.swift
+//  GeoCalculator
+//
+//  Created by Kent Sinclair on 3/29/17.
+//  Copyright © 2017 Jonathan Engelsma. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchViewController: FormViewController {
+
+}
